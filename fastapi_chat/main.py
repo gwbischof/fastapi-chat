2438357d1a246e43a069c3078218615effcea9db
Garrett Bischof <gbischof@bnl.gov>
2021-12-07 21:19:24 -0500

    basic working chat with redis and web sockets

--- fastapi_chat/main.py
+++ fastapi_chat/main.py
@@ -1,15 +1,108 @@
-from fastapi import FastAPI
+#from fastapi import FastAPI
+#import redis
+
+#app = FastAPI()
+#r = redis.Redis(host='localhost', port=7777)
+#stream_name = "garrett"
+
+#@app.get("/post/{message}")
+#async def post(message : str):
+#    r.xadd(stream_name, {'message': message})
+#    return {"message": message}
+
+#@app.get("/read/{last}")
+#async def read(last: int):
+#    return r.xread({'garrett': last})
+
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
 import redis
+import uvicorn
+import asyncio
+import logging
+import json
+
+print("initializing")
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 7777
+XREAD_TIMEOUT = 0
+XREAD_COUNT = 100
+NUM_PREVIOUS = 30
+STREAM_MAX_LEN = 1000
 
 app = FastAPI()
-r = redis.Redis(host='localhost', port=7777)
 stream_name = "garrett"
+r = redis.Redis(host='localhost', port=7777)
 
-@app.get("/post/{message}")
-async def post(message : str):
-    r.xadd(stream_name, {'message': "hello"})
-    return {"message": message}
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-@app.get("/read")
-async def read():
-    return r.xread({'garrett': 0})
+def get_new_messages(last_id):
+    """
+    wait for new items on chat stream and
+    send data from server to client over a WebSocket
+    :param websocket:
+    :type websocket:
+    """
+    # fetch some previous chat history
+    events = r.xread({'garrett': last_id})
+    return events[0][1:][0]
+
+def post_message(message):
+    stream_name = "garrett"
+    # fetch some previous chat history
+    r.xadd(stream_name, {'message': message})
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    last_id = 0
+    while True:
+        data = await websocket.receive_text()
+        print(data)
+        post_message(data)
+        messages = get_new_messages(last_id)
+        for _id, message in messages:
+            await websocket.send_text(str(message))
+            last_id = _id
